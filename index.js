d67737120647472b07d6483e0bdd9eea1c356c7d
Paulo Vitor <vitorpaulo17@hotmail.com>
2021-11-10 16:34:05 -0300

    ajuste do selector agora linkaveis com ajuda de função js

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ function sel1Vl1(){
     alert("Teste de função !!! ");
 
 }
+
+function changeFunc($i) {
+    if ($i == '1'){
+        window.open('https://olhardigital.com.br/','_blank');
+    }else if($i == '2'){
+        window.open('https://g1.globo.com/','_blank');
+    }else if($i == '3'){
+        window.open('https://news.google.com/topstories?hl=pt-BR&gl=BR&ceid=BR:pt-419','_blank');
+    }
+
+}
